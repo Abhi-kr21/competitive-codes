798fe2ac9e8added3a58f95b0ef829bb7c0af25d
Abhi-kr21 <abhishek7858010345@gmail.com>
2022-11-15 21:40:26 +0530

    string folder made

---
+++ String/Reverse_words_in_string.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    string reverseWords(string s)
+    {
+        s += " ";
+        vector<string> st;
+        string temp = "";
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] != ' ')
+            {
+                temp += s[i];
+            }
+            else
+            {
+                st.push_back(temp);
+                temp = "";
+            }
+        }
+        string ans = "";
+        for (auto it = st.rbegin(); it != st.rend(); it++)
+        {
+            if (*it != "")
+            {
+                ans += *it + " ";
+            }
+        }
+        ans.erase(ans.end() - 1);
+        return ans;
+    }
+};
+int main()
+{
+    string s = "";
+    string ans = "";
+    cout << "enter a string" << endl;
+    getline(cin, s);
+    Solution ob;
+    ans = ob.reverseWords(s);
+    cout << ans;
+    return 0;
+}
