2af6d5b384ca15f3715968eab124b3aa52e78871
Abhi-kr21 <abhishek7858010345@gmail.com>
2022-11-19 10:50:53 +0530

    subsequence using recursion

--- Recursion/subsequence_string.cpp
+++ Recursion/subsequence_string.cpp
@@ -4,15 +4,15 @@ using namespace std;
 void is(string op, string ip)
 {
 
-    if (ip.length() == 0)
+    if (ip.length() == 0) // base condition
     {
         cout << op << endl;
         return;
     }
-    string op1 = op;
+    string op1 = op; // here we are making the choice for not taking the element
     string op2 = op;
-    op2.push_back(ip[0]);
-    ip.erase(ip.begin());
+    op2.push_back(ip[0]); // here we are making choice that the element should be included
+    ip.erase(ip.begin()); // here we are making the input small
     is(op1, ip);
     is(op2, ip);
 }
